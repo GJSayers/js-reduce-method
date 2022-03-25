594ef65a862964208725a769bc9ae842970f7815
GJSayers <gemma.sayers1@btinternet.com>
2022-03-25 20:54:51 +0000

    Add grouping and totalling by property

--- reduce.js
+++ reduce.js
@@ -31,7 +31,27 @@ const teamMembers = [
   }
 ];
 
+// Grouping by a property, and totaling it too
+
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log("Total Experience:", totalExperience);
+
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  // check if the accumulator key already exists in the object
+  if (!acc[key]) {
+    // if not, set it equal to the current years experience
+    acc[key] = curr.yrsExperience;
+    // if the key does exist, add the curr years experience
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {});
+console.log("Experience by Profession", experienceByProfession);
+
 // Totaling a specific object property explained
+
 const nums = [0, 1, 2, 3, 4]
 let sum = nums.reduce((acc, curr) => {
 console.log(
@@ -46,7 +66,8 @@ console.log(
 console.log(sum);
 
 // Totaling a specific object property simplified
+
 let sum2 = nums.reduce((acc, curr) => acc + curr, 0);
 console.log(sum2);
 
-// Grouping by a property, and totaling it too
+
